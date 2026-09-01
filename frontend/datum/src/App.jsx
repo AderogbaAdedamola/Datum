c14b239e54b7f1af7e0663d4be9ec9a0c405bb9e
Adedamola <aderogbaadedamola8@gmail.com>
2026-09-01 23:54:26 +0100

    frontend: initial commit

---
+++ frontend/datum/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+
+  return (
+
+    <div className="text-4xl underline text-red-500">
+      Hello Datum
+    </div>
+  )
+}
+
+export default App
